d1b00e5f5e49ba44c58a5f58fa1a15748019df51
niyaak <47914857+niyaak@users.noreply.github.com>
2024-11-01 12:02:26 +0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
-const port = process.env.PORT || 3000;
+const port = 3000;
 const { body, validationResult } = require('express-validator');
 
 const app = express();
